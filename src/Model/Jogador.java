0505605b599c71b2560c29445ee8c7ef7e8fbc33
theogdai <theodorogd@outlook.com>
2018-07-30 22:07:53 -0300

    Alteracoes Classe jogador

--- src/Model/Jogador.java
+++ src/Model/Jogador.java
@@ -1,6 +1,39 @@
 package Model;
 
+import jogo.Jogador;
+
 public class Jogador {
 
-	
+	{
+	    String ID;
+	    Jogador alvo;
+	    boolean vida;
+	    boolean protecao;
+	    boolean acaoValida;
+	    int acao;
+	    boolean balas;
+
+	public Jogador(String nome) {
+		this.ID = nome;
+		this.vida = true;
+		this.protecao = false;
+		this.balas = false;
+		this.acao = 0;
+		this.acaoValida = false;
+	}
+
+	public void defender() {
+		this.protecao = true;
+	}
+
+	public void carregar() {
+		this.balas = true;
+	}
+
+	public void atirar(Jogador atirador, Jogador alvo) {
+		atirador.balas = false;
+		if (!alvo.protecao) {
+			alvo.vida = false;
+		}
+	}
 }
