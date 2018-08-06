e80cf6a6cb0f519645f461211a0fde337db6ab04
x-mizu <40467263+x-mizu@users.noreply.github.com>
2018-08-06 13:01:44 -0300

    Termina de implementar a interface com o usuario

--- src/Interface/Interface_Jogo.java
+++ src/Interface/Interface_Jogo.java
@@ -20,23 +20,30 @@ import Model.Resposta;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
 
 public class Interface_Jogo {
 
 	private JFrame frame;
 	private BlockingQueue<Requisicao> listaRequisicoes;
 	private BlockingQueue<Resposta> listaRespostas;
-	private Resposta respostaInicial;
+
 	private String[][] adversarios; // matriz com Id, cor de avatar dos adversarios, nick e acao
 	private boolean inicioPartida = true; //variavel para limitar a acao do jogador antes do incio da partida
+	private boolean podeRealizarJogada = false;
+	private Requisicao req;
 
 	//interface jogo deve receber uma lista de requisicoes, de respostas e uma resposta inicial
 	public Interface_Jogo(BlockingQueue<Requisicao> listaReq, BlockingQueue<Resposta> listaRes, Resposta respIni) {
 
 		this.listaRequisicoes = listaReq;
 		this.listaRespostas = listaRes;
-		this.respostaInicial = respIni;
+
 		this.adversarios = buscarAdversarios(respIni.jogadores, respIni.jogadorId);
+		
+		this.req = new Requisicao();
+		this.req.servidorId = respIni.servidorId;
+		this.req.jogadorId = respIni.jogadorId;
 
 		initialize();
 		this.frame.setVisible(true);
@@ -44,17 +51,17 @@ public class Interface_Jogo {
 
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 800, 400);
+		frame.setBounds(100, 100, 800, 450);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		//area do registro de acoes, acao atual e quantidade de balas
 		
 		JLabel AcaoAtual = new JLabel("");
+		AcaoAtual.setForeground(Color.RED);
 		
-		JLabel lblRegistroDeAcoes = new JLabel("");
-		
-		JLabel lblQtdDeBalas = new JLabel("Qtd. de Balas:");
+		JTextArea lblRegistroDeAcoes = new JTextArea("");
+		lblRegistroDeAcoes.setEditable(false);
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -108,7 +115,9 @@ public class Interface_Jogo {
 			public void actionPerformed(ActionEvent e) {
 				if (!inicioPartida && !adversarios[1][0].equals("0")) {
 					AcaoAtual.setText(btnAtirarP1.getText() + " em " + adversarios[1][0]);
-					
+					req.acao = Long.valueOf(3);
+					req.jogadorIdAlvo = Long.parseLong(adversarios[0][0]);
+					podeRealizarJogada = true;
 				}
 			}
 		});
@@ -118,6 +127,9 @@ public class Interface_Jogo {
 			public void actionPerformed(ActionEvent e) {
 				if (!inicioPartida && !adversarios[1][1].equals("0")) {
 					AcaoAtual.setText(btnAtirarP2.getText() + " em " + adversarios[1][1]);
+					req.acao = Long.valueOf(3);
+					req.jogadorIdAlvo = Long.parseLong(adversarios[0][1]);
+					podeRealizarJogada = true;
 				}
 			}
 		});
@@ -127,6 +139,9 @@ public class Interface_Jogo {
 			public void actionPerformed(ActionEvent e) {
 				if (!inicioPartida && !adversarios[1][2].equals("0")) {
 					AcaoAtual.setText(btnAtirarP3.getText() + " em " + adversarios[1][2]);
+					req.acao = Long.valueOf(3);
+					req.jogadorIdAlvo = Long.parseLong(adversarios[0][2]);
+					podeRealizarJogada = true;
 				}
 			}
 		});
@@ -136,6 +151,9 @@ public class Interface_Jogo {
 			public void actionPerformed(ActionEvent e) {
 				if (!inicioPartida && !adversarios[1][3].equals("0")) {
 					AcaoAtual.setText(btnAtirarP4.getText() + " em " + adversarios[1][3]);
+					req.acao = Long.valueOf(3);
+					req.jogadorIdAlvo = Long.parseLong(adversarios[0][3]);
+					podeRealizarJogada = true;
 				}
 			}
 		});
@@ -147,16 +165,68 @@ public class Interface_Jogo {
 		btnRecarregar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (!inicioPartida) {
-
+					AcaoAtual.setText(btnRecarregar.getText());
+					req.acao = Long.valueOf(3);
+					podeRealizarJogada = true;
 				}
 			}
 		});
 
-		JButton btnPronto = new JButton("Realizar Jogada");
+		JButton btnPronto = new JButton("Realizar Jogada"); //REALIZA A JOGADA E ATUALIZA A TELA APOS RECEBER RESPOSTA
 		btnPronto.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if (!inicioPartida) {
-
+					while (podeRealizarJogada) {
+						listaRequisicoes.add(req);
+						try { 
+							Resposta resp = listaRespostas.take();
+							if (resp.serverStatus == 6) {
+								if (resp.jogadorId == Long.valueOf(0)) {
+									frame.dispose();
+								} else {
+									adversarios = buscarAdversarios(resp.jogadores, resp.jogadorId); //atualiza matriz de adversarios
+									lblRegistroDeAcoes.setText(resp.acaoesDaJogada); //atualiza registro de acoes
+									
+									//atualia as imagens para a acao do turno anterior
+									try {
+										ImageIcon img1 = new ImageIcon(ImageIO.read(Interface_Inicio.class.getResourceAsStream(
+												Avatar.getCaminhoImg(adversarios[2][0], (int) Long.parseLong(adversarios[3][0])))));
+										System.out.println(adversarios[1][0]);
+										lblPlayer1.setIcon(img1);
+	
+										ImageIcon img2 = new ImageIcon(ImageIO.read(Interface_Inicio.class.getResourceAsStream(
+												Avatar.getCaminhoImg(adversarios[2][1], (int) Long.parseLong(adversarios[3][1])))));
+										System.out.println(adversarios[1][1]);
+										lblPlayer2.setIcon(img2);
+	
+										ImageIcon img3 = new ImageIcon(ImageIO.read(Interface_Inicio.class.getResourceAsStream(
+												Avatar.getCaminhoImg(adversarios[2][2], (int) Long.parseLong(adversarios[3][2])))));
+										System.out.println(adversarios[1][2]);
+										lblPlayer3.setIcon(img3);
+	
+										ImageIcon img4 = new ImageIcon(ImageIO.read(Interface_Inicio.class.getResourceAsStream(
+												Avatar.getCaminhoImg(adversarios[2][3], (int) Long.parseLong(adversarios[3][3])))));
+										System.out.println(adversarios[1][3]);
+										lblPlayer4.setIcon(img4);
+									} catch (IOException e) {
+										e.printStackTrace();
+									}
+									//atualuza textos de nome de adversarios
+									lblP1.setText(adversarios[1][0]);
+									lblP2.setText(adversarios[1][1]);
+									lblP3.setText(adversarios[1][2]);
+									lblP4.setText(adversarios[1][3]);
+									
+									//reseta texto de acao atual
+									AcaoAtual.setText("");
+									
+								}
+								podeRealizarJogada = false;
+							}
+								
+							
+						} catch (InterruptedException e) {e.printStackTrace();}
+					}
 				}
 			}
 		});
@@ -165,7 +235,9 @@ public class Interface_Jogo {
 		btnDefender.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (!inicioPartida) {
-
+					AcaoAtual.setText(btnDefender.getText());
+					req.acao = Long.valueOf(2);
+					podeRealizarJogada = true;
 				}
 			}
 		});
@@ -179,10 +251,7 @@ public class Interface_Jogo {
 		btnIniciarPartida.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				while (inicioPartida) {
-					Requisicao req = new Requisicao();
-					req.servidorId = respostaInicial.servidorId;
 					req.acao = Long.valueOf(7);
-					req.jogadorId = respostaInicial.jogadorId;
 					listaRequisicoes.add(req);
 					
 					try { 
@@ -201,38 +270,37 @@ public class Interface_Jogo {
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(lblP1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(btnAtirarP1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(lblPlayer1, GroupLayout.DEFAULT_SIZE, 175, Short.MAX_VALUE)
-						.addComponent(btnRecarregar, GroupLayout.DEFAULT_SIZE, 175, Short.MAX_VALUE)
-						.addComponent(btnDefender, GroupLayout.DEFAULT_SIZE, 175, Short.MAX_VALUE))
-					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(AcaoAtual, GroupLayout.PREFERRED_SIZE, 378, GroupLayout.PREFERRED_SIZE)
-						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-							.addComponent(lblQtdDeBalas)
-							.addPreferredGap(ComponentPlacement.RELATED, 327, Short.MAX_VALUE)
-							.addComponent(btnPronto, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE))
-						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+								.addComponent(lblP1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(btnAtirarP1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(lblPlayer1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 175, Short.MAX_VALUE)
+								.addComponent(btnRecarregar, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 175, Short.MAX_VALUE)
+								.addComponent(btnDefender, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 175, Short.MAX_VALUE))
+							.addGap(18))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(AcaoAtual, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED)))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblRegistroDeAcoes, GroupLayout.DEFAULT_SIZE, 378, Short.MAX_VALUE)
-								.addGroup(groupLayout.createSequentialGroup()
-									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addComponent(lblPlayer2, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
-										.addComponent(btnAtirarP2, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
-										.addComponent(lblP2, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE))
-									.addGap(18)
-									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addComponent(lblPlayer3, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
-										.addComponent(btnAtirarP3, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
-										.addComponent(lblP3, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE))))
+								.addComponent(lblPlayer2, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnAtirarP2, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblP2, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE))
 							.addGap(18)
-							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-								.addComponent(btnIniciarPartida, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblPlayer4, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
-								.addComponent(btnAtirarP4, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblP4, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE))))
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblPlayer3, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnAtirarP3, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblP3, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)))
+						.addComponent(lblRegistroDeAcoes, GroupLayout.DEFAULT_SIZE, 390, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(btnIniciarPartida, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblPlayer4, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnAtirarP4, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblP4, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnPronto, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE))
 					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
@@ -268,19 +336,21 @@ public class Interface_Jogo {
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addComponent(lblP4, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
 								.addComponent(lblP3, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE))))
-					.addPreferredGap(ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
-					.addComponent(AcaoAtual, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-							.addComponent(btnDefender)
-							.addComponent(btnIniciarPartida))
-						.addComponent(lblRegistroDeAcoes, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnRecarregar)
-						.addComponent(btnPronto)
-						.addComponent(lblQtdDeBalas))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(AcaoAtual, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnDefender)
+								.addComponent(btnIniciarPartida))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnRecarregar)
+								.addComponent(btnPronto)))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblRegistroDeAcoes, GroupLayout.DEFAULT_SIZE, 129, Short.MAX_VALUE)
+							.addGap(5)))
 					.addContainerGap())
 		);
 		frame.getContentPane().setLayout(groupLayout);
@@ -288,7 +358,7 @@ public class Interface_Jogo {
 	}
 
 	public String[][] buscarAdversarios(String[][] jogadores, Long jogadorId) {
-		String[][] adversarios = new String[4][4];
+		String[][] adversarios = new String[5][4];
 		int j = 0;
 		for (int i = 0; i < jogadores[0].length; i++) {
 
@@ -297,6 +367,7 @@ public class Interface_Jogo {
 				adversarios[1][j] = jogadores[1][i];
 				adversarios[2][j] = jogadores[2][i];
 				adversarios[3][j] = jogadores[3][i];
+				adversarios[4][j] = jogadores[4][i];
 				j++;
 			}
 		}
--- src/Principal/JogadorPrincipal.java
+++ src/Principal/JogadorPrincipal.java
@@ -10,6 +10,7 @@ import Model.Requisicao;
 import Model.Resposta;
 import Network.JogadorNetwork;
 
+//ATENCAO, ESSA CLASSE APENAS E RESPOSAVEL POR PEGAR UM OBJETO DE REQUISICAO DA INTERFACE E ENVIAR PARA A CLASSE NETWORK E VICE VERSA
 public class JogadorPrincipal {
 
 	public static void main(String[] args) {
@@ -33,7 +34,7 @@ public class JogadorPrincipal {
 					/// SEMPRE QUE ENVIAR UMA REQUISICAO, FAZER O PROGRAMA ESPERAR PELA RESPOSTA, ASSIM EVITA MERDAS E VARIAVEIS A MAIS
 					//No caso, o Theo entrou na sala, jogador 4
 						
-						String[][] jogadores = {{"1", "2", "3", "4", "0"}, {"Matheus", "Michele", "Nelson", "Theo", "0"}, {"Vermelho", "Azul", "Verde", "Lilas", "0"}, {"0", "0", "0", "0", "0"}};
+						String[][] jogadores = {{"1", "2", "3", "4", "0"}, {"Matheus", "Michele", "Nelson", "Theo", "0"}, {"Vermelho", "Azul", "Verde", "Lilas", "0"}, {"0", "0", "0", "0", "0"}, {"0", "0", "0", "0", "0"}};
 						resp.jogadores = jogadores;
 						
 						resp.serverStatus = Long.valueOf(1);
@@ -53,17 +54,38 @@ public class JogadorPrincipal {
 			
 			try {
 				while(true) {
+					//requisicao de inicio de partida //nao faz muito sentido alterar isso aqui, pq ninguem fez uma acao, nem vai alterar na interface o que vc mudar aqui
 					Requisicao j = listaRequisicoes.take();
 					System.out.println(j.servidorId +"     "+ j.acao + "    " + j.jogadorNickname + "   " + j.jogadorCorAvatar);// teste para ver se a requisicao esta chegando
 					
-						
-						String[][] jogadores = {{"1", "2", "3", "4", "0"}, {"Matheus", "Michele", "Nelson", "Theo", "0"}, {"Vermelho", "Azul", "Verde", "Lilas", "0"}, {"1", "2", "3", "0", "0"}};
-						resp.jogadores = jogadores;
-						
 						resp.serverStatus = Long.valueOf(7); // quando todos os usuarios estao pronto para jogar, envia acao de que o servidor esta em jogo
 						resp.servidorId = Long.valueOf(1);
 						resp.jogadorId = Long.valueOf(4);
 						resp.qtdJogadores = 4;
+						listaRespostas.add(resp);
+						
+					//requisicao de jogadas FIZ DUAS REQUISICOES PARA TESTE, VOCE SO DEVE FAZER PARA ELE PEGAR UMA REQUISICAO E ENVIAR UMA RESPOSTA, POIS ESTA EM LOOP
+						Requisicao j2 = listaRequisicoes.take();
+						System.out.println(j.servidorId +"     "+ j.acao + "    " + j.jogadorId + "   " + j.jogadorIdAlvo);// teste para ver se a requisicao esta chegando
+						
+						
+						String[][] jogadores2 = {{"0", "2", "3", "4", "0"}, {"Theo", "Michele", "Nelson", "0", "0"}, {"0", "Azul", "Verde", "Lilas", "0"}, {"3", "1", "2", "3", "0"}, {"0", "1", "0", "0", "0"}};
+						resp.jogadores = jogadores2;
+						resp.serverStatus = Long.valueOf(6); //resultado do turno 
+						resp.servidorId = Long.valueOf(1); 
+						resp.jogadorId = Long.valueOf(4);
+						resp.qtdJogadores = 3; //neste turno simleu que um jogador morreu (no caso, eu)
+						resp.acaoesDaJogada = "Michele atirou em Matheus\n" + "Matheus morreu\n"+"Theo recarregou\n"+"Nelson defendeu\n";
+						
+
+					/*	String[][] jogadores2 = {{"1", "2", "3", "0", "0"}, {"Matheus", "Michele", "Nelson", "0", "0"}, {"Vermelho", "Azul", "Verde", "0", "0"}, {"2", "1", "2", "2", "0"}, {"0", "4", "0", "0", "0"}};
+						resp.jogadores = jogadores2;
+						resp.serverStatus = Long.valueOf(6); //resultado do turno 
+						resp.servidorId = Long.valueOf(1); 
+						resp.jogadorId = Long.valueOf(0);
+						resp.qtdJogadores = 3; //neste turno simleu que um jogador morreu (no caso, o Theo, ou seja, este jogador)
+						resp.acaoesDaJogada = "Michele atirou em Theo\n" + "Matheus defendeu\n"+"Theo morreu\n"+"Nelson defendeu\n";*/
+						
 						listaRespostas.add(resp);
 					}
 			} catch (InterruptedException e) { e.printStackTrace(); }
